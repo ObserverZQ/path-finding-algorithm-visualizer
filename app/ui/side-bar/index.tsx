8ed4cb8db016e217d811c3f13754797fd017cb6f
ObserverZQ <657282348@qq.com>
2025-11-24 03:23:10 +0800

    feat(ui): added headlessui for customization

--- app/ui/side-bar/index.tsx
+++ app/ui/side-bar/index.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { useCallback } from 'react';
-import { Button } from 'flowbite-react';
+// import { Button } from 'flowbite-react';
+import { Button } from '@headlessui/react';
 import { useBearStore } from '@/app/lib/store';
-
+import Counter from './counter';
 export default function SideBar() {
   const increase = useBearStore((state) => state.increase);
   const increasePopulation = () => {
@@ -15,11 +16,15 @@ export default function SideBar() {
 
   return (
     <div className='w-[378px] h-full bg-amber-100'>
-      <Button onClick={increasePopulation}>Run</Button>
+      <Button className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-active:bg-sky-700 data-hover:bg-sky-500" onClick={increasePopulation}>Run</Button>
+      <Button className="rounded bg-sky-600 px-4 py-2 text-sm text-white data-active:bg-sky-700 data-hover:bg-sky-500">
+        Save changes
+      </Button>
+      <Counter />
     </div>
   );
 }
-import Counter from './counter';
+
 
 // export default function SideBar() {
 //     return <div><Counter /></div>
