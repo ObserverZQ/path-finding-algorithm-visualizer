4c4e7f2ff27fec0abcde280ad41b0b9222b57fbb
ObserverZQ <657282348@qq.com>
2026-01-13 16:36:16 +0800

    feat(style): add overflow auto to adjust to height overflow

--- app/page.tsx
+++ app/page.tsx
@@ -7,13 +7,16 @@ export default async function Page() {
   return (
     <div className='w-full flex flex-col items-center bg-neutral-50'>
       <Header />
-      <div className='w-[1440px] flex justify-between h-[calc(100vh-60px)]'>
-        <div className='flex flex-col gap-5'>
-          <MazeLoader />
-          <Trace />
+      <div className='w-full flex justify-center overflow-auto'>
+        <div className='w-[1440px] flex justify-between h-[calc(100vh-60px)]'>
+          <div className='flex flex-col gap-5'>
+            <MazeLoader />
+            <Trace />
+          </div>
+          <SideBar />
         </div>
-        <SideBar />
       </div>
+
     </div>
   );
 }
