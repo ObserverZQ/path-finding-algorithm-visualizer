6e0723360bae5eac1298e753ba72089ff2e38d7b
ObserverZQ <657282348@qq.com>
2026-01-20 22:35:31 +0800

    feat(doc): add mdx support for learning

--- app/ui/side-bar/algorithm-selection.tsx
+++ app/ui/side-bar/algorithm-selection.tsx
@@ -1,13 +1,20 @@
 'use client';
 import { useState } from 'react';
-import { TextInput, Checkbox, Label, Button, Drawer, DrawerHeader, DrawerItems } from 'flowbite-react';
 import {
+  TextInput,
+  Checkbox,
+  Label,
+  Button,
+  Drawer,
+  DrawerHeader,
+  DrawerItems,
   Accordion,
   AccordionContent,
   AccordionPanel,
   AccordionTitle,
   Radio,
 } from 'flowbite-react';
+import { VscBook } from 'react-icons/vsc';
 // import { Transition, Field, Radio, RadioGroup } from '@headlessui/react';
 import {
   Algorithm,
@@ -18,6 +25,7 @@ import {
   defaultHeuristic,
   useSideBarStore,
 } from '@/app/lib/sidebar';
+import Hello from '@/app/docs/hello.mdx';
 
 const plans = ['Manhattan', 'Euclidean', 'Octile', 'Chebyshev'];
 
@@ -38,7 +46,10 @@ export default function AlgorithmSelection() {
     setAlgorithm(alg);
     console.log('Selected algorithm:', alg);
   };
-
+  const onClickLearn = () => {
+    console.log('Learn about', algorithm);
+    setIsOpen(true);
+  };
   const handleOptionChange = (
     alg: AlgorithmTypeKey,
     key: string,
@@ -148,51 +159,63 @@ export default function AlgorithmSelection() {
                   </div>
                 );
               })}
-              <Button outline pill size="sm" className='mt-2 text-primary-500 border-primary-500'>Learn About {algorithm.name}</Button>
+              <Button
+                outline
+                pill
+                size='sm'
+                className='mt-2 text-primary-500 border-primary-500'
+                onClick={onClickLearn}
+              >
+                Learn About {algorithm.name}
+              </Button>
             </AccordionContent>
           </AccordionPanel>
         ))}
       </Accordion>
-      <Drawer open={isOpen} onClose={handleClose} position="right">
-        <DrawerHeader title="Drawer" />
+      <Drawer open={isOpen} onClose={handleClose} position='right'>
+        <DrawerHeader title={algorithm.name} titleIcon={VscBook} />
         <DrawerItems>
-          <p className="mb-6 text-sm text-gray-500 dark:text-gray-400">
+          <Hello />
+          {/* <p className='mb-6 text-sm text-gray-500 dark:text-gray-400'>
             Supercharge your hiring by taking advantage of our&nbsp;
-            <a href="#" className="text-cyan-600 underline hover:no-underline dark:text-cyan-500">
+            <a
+              href='#'
+              className='text-cyan-600 underline hover:no-underline dark:text-cyan-500'
+            >
               limited-time sale
             </a>
-            &nbsp;for Flowbite Docs + Job Board. Unlimited access to over 190K top-ranked candidates and the #1 design
-            job board.
-          </p>
-          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            &nbsp;for Flowbite Docs + Job Board. Unlimited access to over 190K
+            top-ranked candidates and the #1 design job board.
+          </p> */}
+          {/* <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
             <a
-              href="#"
-              className="rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700"
+              href='#'
+              className='rounded-lg border border-gray-200 bg-white px-4 py-2 text-center text-sm font-medium text-gray-900 hover:bg-gray-100 hover:text-cyan-700 focus:z-10 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:border-gray-600 dark:bg-gray-800 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white dark:focus:ring-gray-700'
             >
               Learn more
             </a>
             <a
-              href="#"
-              className="inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800"
+              href='#'
+              className='inline-flex items-center rounded-lg bg-cyan-700 px-4 py-2 text-center text-sm font-medium text-white hover:bg-cyan-800 focus:outline-none focus:ring-4 focus:ring-cyan-300 dark:bg-cyan-600 dark:hover:bg-cyan-700 dark:focus:ring-cyan-800'
             >
               Get access&nbsp;
               <svg
-                className="ms-2 h-3.5 w-3.5 rtl:rotate-180"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 14 10"
+                className='ms-2 h-3.5 w-3.5 rtl:rotate-180'
+                aria-hidden='true'
+                xmlns='http://www.w3.org/2000/svg'
+                fill='none'
+                viewBox='0 0 14 10'
               >
                 <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M1 5h12m0 0L9 1m4 4L9 9"
+                  stroke='currentColor'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
+                  d='M1 5h12m0 0L9 1m4 4L9 9'
                 />
               </svg>
             </a>
-          </div>
+          </div> */}
         </DrawerItems>
       </Drawer>
     </div>
--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
-import type { NextConfig } from "next";
-import withFlowbiteReact from "flowbite-react/plugin/nextjs";
+import type { NextConfig } from 'next';
+import withFlowbiteReact from 'flowbite-react/plugin/nextjs';
+import createMDX from '@next/mdx';
+
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+  extension: /\.(md|mdx)$/,
+});
 
 const nextConfig: NextConfig = {
-    /* config options here */
+  /* config options here */
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
 };
 
-export default withFlowbiteReact(nextConfig);
+export default withMDX(withFlowbiteReact(nextConfig));
